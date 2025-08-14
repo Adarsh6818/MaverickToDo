71cb10cb48f82eaed3647edf0ceaee277c6ad548
Adarsh6818 <adarshvish01@gmail.com>
2025-08-15 01:02:39 +0530

    Added Javascript functionality

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-const a = 15;
-a;
+
+document.querySelector('#push').onclick = () => {
+
+    // Adding validation for emply input field
+    if(document.querySelector('#newTask input').value.length == 0){
+        alert("Please enter a task")
+    }
+
+    //If user types any task
+    else {
+        document.querySelector('#tasks').innerHTML += `
+            <div class="task">
+                <span id"taskname">
+                    ${document.querySelector('#newTask input').value}
+                </span>
+                  <button class="delete">
+                <i class="far fa-trash-alt"></i>
+            </button>
+            </div>
+          
+            
+        `
+        
+    }
+}
+
