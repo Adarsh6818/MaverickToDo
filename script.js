faa0ce54ade60cd89e7785dc1608eb65e3816930
Adarsh6818 <adarshvish01@gmail.com>
2025-08-18 12:55:14 +0530

    Delete button function added

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
+document.querySelector("#push").onclick = () => {
+  // Adding validation for emply input field
+  if (document.querySelector("#newTask input").value.length == 0) {
+    alert("Please enter a task");
+  }
 
-document.querySelector('#push').onclick = () => {
-
-    // Adding validation for emply input field
-    if(document.querySelector('#newTask input').value.length == 0){
-        alert("Please enter a task")
-    }
-
-    //If user types any task
-    else {
-        document.querySelector('#tasks').innerHTML += `
+  //If user types any task
+  else {
+    document.querySelector("#tasks").innerHTML += `
             <div class="task">
                 <span id"taskname">
-                    ${document.querySelector('#newTask input').value}
+                    ${document.querySelector("#newTask input").value}
+                
                 </span>
                   <button class="delete">
+               
                 <i class="far fa-trash-alt"></i>
             </button>
             </div>
-          
-            
-        `
-        
-    }
-}
+          `;
+    console.log("Adarsh");
 
+    let current_tasks = document.querySelectorAll(".delete");
+    for (let i = 0; i < current_tasks.length; i++) {
+      current_tasks[i].onclick = function () {
+        this.parentNode.remove();
+      };
+    }
+
+    let tasks = document.querySelectorAll(".task");
+    for (let i = 0; i < tasks.length; i++) {
+      tasks[i].onclick = function () {
+        this.classList.toggle("completed");
+      };
+    }
+
+    document.querySelector("#newTask input").value = "";
+  }
+};
